e38dff13cddabba1e00b58db379e214b43a0dfe6
Bolaji5821 <oyedelebolaji84@gmail.com>
2023-01-06 17:02:45 +0100

    sum of int and float using List and Union

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,11 +1,10 @@
-#/usr/bin/env bash
-"""returns some of integers and float
-"""
-
+#!/usr/bin/env python3
+'''returns some of int and float
+'''
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
-    '''returns the sum of list of ints and floating-point number
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    '''Computes the sum of a list of integers and floating-point numbers.
     '''
-    return float(sum(mxd_list))
+    return float(sum(mxd_lst))
